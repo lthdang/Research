2b19078ebc33875d23c3dc4bfd4707cf5a2e0a04
Hai Dang <lthdang@ninepoints.vn>
2023-11-13 11:52:15 +0700

    - update bootrap for all views

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -8,25 +8,25 @@
     <meta name="generator" content="Hugo 0.87.0">
     <title>@yield('title')</title>
 
-    <script src="..\assets\vendor\hs-navbar-vertical-aside\hs-navbar-vertical-aside-mini-cache.js"></script>
-    <link rel="shortcut icon" href="..\favicon.ico">
+    <script src="{{ asset('assets/vendor/hs-navbar-vertical-aside/hs-navbar-vertical-aside-mini-cache.js') }}"></script>
+    <link rel="shortcut icon" href="{{asset('/favicon.ico')}}">
 
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
 
     <!-- CSS Implementing Plugins -->
-    <link rel="stylesheet" href="..\assets\css\vendor.min.css">
-    <link rel="stylesheet" href="..\assets\vendor\icon-set\style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/vendor.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/vendor/icon-set/style.css')}}">
 
     <!-- CSS Front Template -->
-    <link rel="stylesheet" href="..\assets\css\theme.min.css?v=1.0">
+    <link rel="stylesheet" href="{{asset('assets/css/theme.min.css?v=1.0')}}">
 
     <!-- Bootstrap core CSS -->
-    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+    <link href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="{!! url('assets/css/app.css') !!}" rel="stylesheet">
+    <link href="{{asset('assets/css/app.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link href="{!! url('assets/css/custom.css') !!}" rel="stylesheet">
+    <link href="{{asset('assets/css/custom.css') }}" rel="stylesheet">
 
     <script>
         $(document).ready(function () {
