b36e71e6d5e0baf526593f1c8aeed75d849a85b5
Hai Dang <lthdang@ninepoints.vn>
2024-01-24 09:19:57 +0700

    - update blog

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.auth-master')
 @section('title','Blog-Login')
 @section('content')
-    <form method="post" action="{{ route('login.perform') }}">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Styles -->
+    <form method="post" action="{{url('captcha-validation')}}">
         @csrf
         <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
         <img class="mb-4" src="{!! url('images/blog.svg') !!}" alt="" width="250" height="100">
-
         <h1 class="h3 mb-3 fw-normal">Login</h1>
-
         @include('layouts.partials.messages')
-
         <div class="form-group form-floating mb-3">
             <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username"
                    required="required" autofocus>
@@ -27,15 +28,31 @@
                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
             @endif
         </div>
-
-        <div class="form-group mb-3">
+        <div class="form-group mt-4 mb-4">
+            <div class="captcha">
+                <span id="captcha-img">{!! captcha_img() !!}</span>
+                <button type="button" class="btn btn-danger" class="reload" id="reload">
+                    ↻
+                </button>
+            </div>
+        </div>
+        <div class="form-group mb-3 ">
             <input type="checkbox" name="remember" value="1">
             <label for="remember">Remember me</label>
             <a href="{{ route('register.perform') }}">Sign-up</a>
         </div>
-
         <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-
         <p class="mt-5 mb-3 text-muted">&copy; {{date('Y')}}</p>
     </form>
+    <script type="text/javascript">
+        $('#reload').click(function () {
+            $.ajax({
+                type: 'GET',
+                url: 'reload-captcha',
+                success: function (data) {
+                    $('#captcha-img').html(data.captcha);
+                }
+            });
+        });
+    </script>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::resource('blogs', BlogController::class);
+Route::get("get", [BlogController::class,'list']);
+Route::post("add", [BlogController::class,'add']);
+Route::put("update/{id}", [BlogController::class,'update']);
+
